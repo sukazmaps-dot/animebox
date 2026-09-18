954e72c24b0475689683a77d58980eeaae12216f
kazumi <sukazmaps@gmail.com>
2026-09-18 18:53:11 +0500

    Add Yandex Metrika analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -242,9 +242,49 @@ export default function RootLayout({
           src="https://telegram.org/js/telegram-web-app.js"
           strategy="beforeInteractive"
         />
+
+        {/* Yandex.Metrika counter 112789274 */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112789274', 'ym');
+
+              ym(112789274, 'init', {
+                ssr: true,
+                webvisor: true,
+                clickmap: true,
+                ecommerce: "dataLayer",
+                referrer: document.referrer,
+                url: location.href,
+                accurateTrackBounce: true,
+                trackLinks: true
+              });
+            `,
+          }}
+        />
       </head>
 
       <body>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/112789274"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+              aria-hidden="true"
+            />
+          </div>
+        </noscript>
+
         {/* Website structured data */}
 
         <script
