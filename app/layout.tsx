8f62bea20da2e9355799083609424d40b235554f
kazumi <sukazmaps@gmail.com>
2026-09-18 21:33:51 +0500

    Load Premium styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import './card-layout.css';
 import './smart-home.css';
 import './monetization.css';
 import './boosty-support.css';
+import './premium.css';
 import './donatepay-claim.css';
 import './sponsor-v2.css';
 import './monetization-v3.css';
