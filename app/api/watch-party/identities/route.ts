a70cbaa9261ea1fae499bb406ec45f3f9603f2a6
kazumi <sukazmaps@gmail.com>
2026-09-21 16:53:48 +0500

    Allow identity hydration for 50-person Watch Together rooms

--- app/api/watch-party/identities/route.ts
+++ app/api/watch-party/identities/route.ts
@@ -12,7 +12,7 @@ import { getSponsorStatuses } from '@/lib/sponsor-server';
 
 const UUID_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-const MAX_IDENTITIES = 8;
+const MAX_IDENTITIES = 50;
 
 type ProfileIdentityRow = {
   id: string;
