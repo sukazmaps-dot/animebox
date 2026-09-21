9f676e40a2002370d1400cd9545d93ebc5a16505
kazumi <sukazmaps@gmail.com>
2026-09-21 14:26:17 +0500

    Allow personal schedule product events

--- lib/product-events-server.ts
+++ lib/product-events-server.ts
@@ -22,6 +22,8 @@ export const PRODUCT_CLIENT_EVENT_NAMES = [
   'continue_watching_click',
   'continue_watching_started',
   'personal_home_view',
+  'personal_schedule_impression',
+  'personal_schedule_click',
   'notification_center_open',
   'notification_subscription_toggle',
   'recommendation_impression',
