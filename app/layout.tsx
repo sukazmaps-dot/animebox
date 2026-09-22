202a0b6111e18d255da020b41db80795b80a7128
kazumi <sukazmaps@gmail.com>
2026-09-23 03:55:29 +0500

    Fix mobile anime detail personal blocks

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,7 @@ import './patch12-6-mobile-home-final.css';
 import './patch12-6-1-mobile-hero.css';
 import './patch12-6-2-home-rails-typography.css';
 import './patch12-6-3-mobile-nav-home-cleanup.css';
+import './patch12-6-4-anime-detail-mobile-cleanup.css';
 
 import TelegramMiniAppBridge from '@/components/TelegramMiniAppBridge';
 import TelegramSubscriptionGate from '@/components/TelegramSubscriptionGate';
