f0e2de13d4029e6d33f5e7efabc54b0c01698869
kazumi <sukazmaps@gmail.com>
2026-09-23 18:56:09 +0500

    Activate Episode Identity layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,7 @@ import './patch14-1-visual-foundation.css';
 import './patch14-1-1-anti-ai-design.css';
 import './patch14-2-mobile-shell.css';
 import './patch14-3-home-discovery.css';
+import './patch14-5-episode-identity.css';
 
 import TelegramMiniAppBridge from '@/components/TelegramMiniAppBridge';
 import TelegramSubscriptionGate from '@/components/TelegramSubscriptionGate';
