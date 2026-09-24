e226f6c62b19183e9ecf4a6feec15e1019aaeb24
kazumi <sukazmaps@gmail.com>
2026-09-24 14:50:09 +0500

    Patch 16.3: add player source control page

---
+++ app/admin/player-sources/page.tsx
@@ -0,0 +1,9 @@
+import PlayerSourceControl from '@/components/admin/PlayerSourceControl';
+
+export default function PlayerSourcesAdminPage() {
+  return (
+    <main>
+      <PlayerSourceControl />
+    </main>
+  );
+}
