0474344f65a20da49e3f139c0d081066ee4439df
kazumi <sukazmaps@gmail.com>
2026-09-24 13:24:35 +0500

    Patch 16.2: add copyright admin page

---
+++ app/admin/copyright/page.tsx
@@ -0,0 +1,9 @@
+import CopyrightAdminClient from '@/components/admin/CopyrightAdminClient';
+
+export default function CopyrightAdminPage() {
+  return (
+    <main>
+      <CopyrightAdminClient />
+    </main>
+  );
+}
